0e69a64ad69b159b30e2824fc9c742d2dc5a40fd
Gilles Dubreuil <gilles@redhat.com>
2018-07-26 13:43:01 +1000

    Infra Discovery: Add SSL port

--- app/models/manageiq/providers/openstack/discovery.rb
+++ app/models/manageiq/providers/openstack/discovery.rb
@@ -2,18 +2,23 @@ require 'manageiq/network_discovery/port'
 
 module ManageIQ::Providers::Openstack
   class Discovery
-    IRONIC_PORT = 6385
+    IRONIC_PORTS = [13385, 6385]
 
     def self.probe(ost)
       # Openstack InfraManager (TripleO/Director) discovery
-      if ManageIQ::NetworkDiscovery::Port.open?(ost, IRONIC_PORT)
-        res = ""
-        Socket.tcp(ost.ipaddr, 6385) do |s|
-          s.print("GET / HTTP/1.0\r\n\r\n")
-          s.close_write
-          res = s.read
+      IRONIC_PORTS.each do |port|
+        if ManageIQ::NetworkDiscovery::Port.open?(ost, port)
+          res = ''
+          Socket.tcp(ost.ipaddr, port) do |s|
+            s.print("GET / HTTP/1.0\r\n\r\n")
+            s.close_write
+            res = s.read
+          end
+          if res =~ /OpenStack Ironic API/
+            ost.hypervisor << :openstack_infra
+            break
+          end
         end
-        ost.hypervisor << :openstack_infra if res =~ /OpenStack Ironic API/
       end
     end
   end
